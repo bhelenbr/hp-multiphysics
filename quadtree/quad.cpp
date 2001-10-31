60fe4d565cf4a5063b056f8e35cdeb2e98fced74
helenbrk <helenbrk@346d1d51-7a3b-0410-9033-b9cd4ee96340>
2001-10-31 15:41:24 +0000

    putting quadtree under cvs

---
+++ quadtree/quad.cpp
@@ -0,0 +1,367 @@
+/*
+ *  quad.cpp
+ *  mblock
+ *
+ *  Created by helenbrk on Wed Aug 08 2001.
+ *  Copyright (c) 2001 __CompanyName__. All rights reserved.
+ *
+ */
+ #include"quadtree.h"
+ #include<utilities.h>
+ #include<float.h>
+ #include<cstdio>
+ #include<assert.h>
+  
+void quadtree::init(FLT (*v)[ND], int mxv, FLT xmin, FLT ymin, FLT xmax, FLT ymax) {
+   int i;
+   
+   vrtx = v;
+   maxvrtx = mxv;
+   if (size != 0) {
+      delete [] base;
+      delete [] indx;
+   }
+/*	TAKES APPROXIMATELY 1:1 QUADS TO VERTICES WITH 4 NODES / QUAD */
+/*	+10 IS FACTOR OF SAFETY FOR SMALL mxv */
+   size = (int) 1.1*mxv +10; 
+   base = new class quad[size];
+   indx = new class quad*[maxvrtx];
+   for(i=0;i<maxvrtx;++i)
+      indx[i] = NULL;
+   base[0] = quad(NULL,0,xmin,ymin,xmax,ymax);
+   current = 1;
+}
+
+void quadtree::init(FLT xmin, FLT ymin, FLT xmax, FLT ymax) {
+   base[0] = quad(NULL,0,xmin,ymin,xmax,ymax);
+
+   for(int i = 0; i<maxvrtx; ++i)
+      indx[i] = NULL;
+      
+   current = 1;
+}
+
+quadtree& quadtree::operator=(const quadtree& copy) {
+   int i,j;
+      
+   vrtx = copy.vrtx;
+   if (size == 0) {
+      maxvrtx = copy.maxvrtx;
+      size = copy.size;
+      base = new class quad[size];
+      indx = new class quad*[maxvrtx];
+   }
+   else {
+      assert(size >= copy.size);
+      assert(maxvrtx >= copy.maxvrtx);
+   }
+
+   for(i=0;i<maxvrtx;++i) {
+      if (copy.indx[i] == NULL) indx[i] = NULL;
+      else indx[i] = base + (copy.indx[i] -copy.base);
+   }
+
+   current = copy.current;
+   
+   for(i=0;i<current;++i) {
+      base[i].num = copy.base[i].num;
+      base[i].prnt = base +(copy.base[i].prnt -copy.base);
+      base[i].pind = copy.base[i].pind;
+      base[i].xmin = copy.base[i].xmin;
+      base[i].ymin = copy.base[i].ymin;
+      base[i].xmax = copy.base[i].xmax;
+      base[i].ymax = copy.base[i].ymax;
+      if (base[i].num > 0) {
+         for(j=0;j<4;++j)
+            base[i].node[j] = copy.base[i].node[j];
+      }
+      else {
+         for(j=0;j<4;++j)
+            base[i].dghtr[j] = base +(copy.base[i].dghtr[j] -copy.base);
+      }
+   }
+   
+   base[0].prnt = NULL;
+   
+   return(*this);
+}
+
+void quadtree::addpt(int v0, class quad* start = NULL) {
+   int i,j;  // DON'T MAKE THESE STATIC SCREWS UP RECURSION
+   class quad *qpt;  // SAME
+   static int store[5];
+   static FLT avoid0,xshift,yshift,x1,y1,dx,dy;
+   
+   if (start == NULL) 
+      qpt = base;
+   else
+      qpt = start;
+
+/*	LOOP TO BOTTOM OF TREE */
+   while (qpt->num < 0) {
+      xshift = vrtx[v0][0] -0.5*(qpt->xmax +qpt->xmin);
+      avoid0 = MAX(fabs(xshift), 10.*FLT_EPSILON);
+      i = ((int) ((1+10.*FLT_EPSILON)*xshift/avoid0) +1)/2;
+      yshift = vrtx[v0][1] -0.5*(qpt->ymax +qpt->ymin);
+      avoid0 = MAX(fabs(yshift), 10.*FLT_EPSILON);
+      i = 2*i +((int) ((1+10.*FLT_EPSILON)*yshift/avoid0) +1)/2;
+      qpt = qpt->dghtr[i];
+   }
+   
+   if (qpt->num < 4) {
+/*		QUAD CAN ACCEPT NEW POINT */
+      qpt->node[qpt->num++] = v0;
+      indx[v0] = qpt;
+      return;
+   }
+
+/*	QUAD IS FULL SUBDIVIDE */
+   if (current +4 >= size) {
+      printf("Need to allocate bigger quadtree %d\n",size);
+      exit(1);
+   }
+   for (i=0;i<4;++i)
+      store[i] = qpt->node[i];
+   store[4] = v0;
+   
+   qpt->num = -1;
+   dx = (qpt->xmax -qpt->xmin)*0.5;
+   dy = (qpt->ymax -qpt->ymin)*0.5;
+      
+   for (i=0;i<2;++i) {
+      for(j=0;j<2;++j) {
+         x1 = qpt->xmin +dx*i;
+         y1 = qpt->ymin +dy*j;
+         base[current] = quad(qpt,2*i +j,x1,y1,x1+dx,y1+dy);
+         qpt->dghtr[2*i +j] = base +current;
+         ++current;
+      }
+   }
+   
+   for(i=0;i<5;++i)
+      addpt(store[i],qpt);
+      
+   return;
+}
+
+/*	FIND CLOSEST POINT TO A POINT IN THE ARRAY */
+FLT quadtree::nearpt(int v0, int& pt) const {
+   int i,nsrch,exclude;
+   class quad *srchlst[100], *topbox;
+   FLT dist,dx,dy,xh,xl,yh,yl,mindist;
+   class quad *qpt;
+
+   xl = base[0].xmin;
+   xh = base[0].xmax;
+   yl = base[0].ymin;
+   yh = base[0].ymax;
+   mindist = (xh -xl)*(xh -xl) +(yh-yl)*(yh-yl);
+   nsrch = 0;
+   
+   if (indx[v0] == NULL) 
+      srchlst[nsrch++] = base;
+   else
+      srchlst[nsrch++] = indx[v0];
+      
+   topbox = srchlst[0];	 
+
+   for(;;) {
+      while (nsrch > 0) {
+         qpt = srchlst[--nsrch];
+         if (qpt->xmin > xh || qpt->xmax < xl || qpt->ymin > yh || qpt->ymax < yl) continue;
+         if (qpt->num < 0) {
+/*				ADD DAUGHTERS TO LIST */
+            for(i=0;i<4;++i)
+               srchlst[nsrch++] = qpt->dghtr[i];
+            continue;
+         }
+         else {
+/*				CHECK POINTS */
+            for(i=0;i<qpt->num;++i) {
+               if (qpt->node[i] == v0) continue;
+               dx = vrtx[v0][0] -vrtx[qpt->node[i]][0];
+               dy = vrtx[v0][1] -vrtx[qpt->node[i]][1];
+               dist = dx*dx + dy*dy;
+               if (dist < mindist) {
+                  pt = qpt->node[i];
+                  mindist = dist;
+                  dist = sqrt(dist);
+                  xh = vrtx[v0][0] +dist;
+                  xl = vrtx[v0][0] -dist;
+                  yh = vrtx[v0][1] +dist;
+                  yl = vrtx[v0][1] -dist;
+               }
+            }
+         }
+      }
+      if (topbox->xmin < xl && topbox->xmax > xh && 
+            topbox->ymin < yl && topbox->ymax > yh) return(mindist);
+      
+      exclude = topbox->pind;
+      topbox = topbox->prnt;
+
+/*		CHECK IF WE ARE AT THE TOP LEVEL */
+      if (topbox == NULL) return(mindist);
+      
+      for(i=0;i<4;++i)
+         if (i != exclude) srchlst[nsrch++] = topbox->dghtr[i];
+   }
+
+   return(mindist);
+}
+
+/* FIND CLOSEST POINT TO AN ARBITRARY POINT */
+FLT quadtree::nearpt(FLT x, FLT y, int& pt) const {
+   int i,nsrch,exclude;
+   class quad *srchlst[100], *topbox;
+   FLT dist,dx,dy,xh,xl,yh,yl,mindist;
+   class quad *qpt;
+
+   xl = base[0].xmin;
+   xh = base[0].xmax;
+   yl = base[0].ymin;
+   yh = base[0].ymax;
+   mindist = (xh -xl)*(xh -xl) +(yh-yl)*(yh-yl);
+   
+   nsrch = 0;
+   srchlst[nsrch++] = base;
+   topbox = srchlst[0];	 
+
+   for(;;) {
+      while (nsrch > 0) {
+         qpt = srchlst[--nsrch];
+         if (qpt->xmin > xh || qpt->xmax < xl || qpt->ymin > yh || qpt->ymax < yl) continue;
+         if (qpt->num < 0) {
+/*				ADD DAUGHTERS TO LIST */
+            for(i=0;i<4;++i)
+               srchlst[nsrch++] = qpt->dghtr[i];
+            continue;
+         }
+         else {
+/*				CHECK POINTS */
+            for(i=0;i<qpt->num;++i) {
+               dx = x -vrtx[qpt->node[i]][0];
+               dy = y -vrtx[qpt->node[i]][1];
+               dist = dx*dx + dy*dy;
+               if (dist < mindist) {
+                  pt = qpt->node[i];
+                  mindist = dist;
+                  dist = sqrt(mindist);
+                  xh = x +dist;
+                  xl = x -dist;
+                  yh = y +dist;
+                  yl = y -dist;
+               }
+            }
+         }
+      }
+      if (topbox->xmin < xl && topbox->xmax > xh && 
+            topbox->ymin < yl && topbox->ymax > yh) return(mindist);
+      
+      exclude = topbox->pind;
+      topbox = topbox->prnt;
+
+/*		CHECK IF WE ARE AT THE TOP LEVEL */
+      if (topbox == NULL) return(mindist);
+      
+      for(i=0;i<4;++i)
+         if (i != exclude) srchlst[nsrch++] = topbox->dghtr[i];
+   }
+
+   return(mindist);
+}
+
+void quadtree::dltpt(int v0) {
+   int ind, i;
+   class quad *qpt;
+   
+   qpt = indx[v0];
+   
+   if (qpt == NULL) {
+      printf("error: deleting point which isn't in tree %d\n",v0);
+      exit(1);
+   }
+   
+   indx[v0] = NULL;
+
+   ind = -1;
+   for(i=0;i<qpt->num;++i) {
+      if (qpt->node[i] == v0) {
+         ind = i;
+         break;
+      }
+   }
+   
+   if (ind == -1) {
+      printf("Couldn't find node to remove: %d\n",v0);
+      exit(1);
+   }
+   
+   for(i=ind+1;i<qpt->num;++i)
+      qpt->node[i-1] = qpt->node[i];
+   
+   --qpt->num;
+   
+   return;
+}
+
+void quadtree::output() {
+   int i,nsrch;
+   class quad *srchlst[100];
+   class quad *qpt;
+   
+   nsrch = 0;
+   srchlst[nsrch++] = base;
+   
+   while (nsrch > 0) {
+      qpt = srchlst[--nsrch];
+      if (qpt->num < 0) {
+/*			ADD DAUGHTERS TO LIST */
+         for(i=0;i<4;++i)
+            srchlst[nsrch++] = qpt->dghtr[i];
+         continue;
+      }
+      else {
+/*			PRINT POINTS */
+         printf("box: %f %f %f %f\n",qpt->xmin,qpt->xmax,qpt->ymin,qpt->ymax);
+         for(i=0;i<qpt->num;++i) 
+            printf("%d %f %f\n",qpt->node[i],vrtx[qpt->node[i]][0],vrtx[qpt->node[i]][1]);
+      }
+   }
+   
+   return;
+}
+
+void quadtree::movept(int from, int to) {
+   int i;
+   class quad *qpt;
+   
+   qpt = indx[from];
+   
+   if (qpt == NULL) {
+      printf("error: moving point which isn't in tree %d\n",from);
+      exit(1);
+   }
+   
+   if (indx[to] != NULL) {
+      printf("error: moving to a point which exists %d\n",to);
+      exit(1);
+   }
+   
+   indx[to] = indx[from];
+   indx[from] = NULL;
+
+   for(i=0;i<qpt->num;++i) {
+      if (qpt->node[i] == from) {
+         qpt->node[i] = to;
+         break;
+      }
+   }
+   assert(i != qpt->num);
+
+   return;
+}
+
+
+   
+   
---
+++ quadtree/quadtree.h
@@ -0,0 +1,65 @@
+/*
+ *  quadtree.h
+ *  mblock
+ *
+ *  Created by helenbrk on Thu Aug 09 2001.
+ *  Copyright (c) 2001 __CompanyName__. All rights reserved.
+ *
+ */
+
+#include<cstdio>
+#ifndef _quadtree_h_
+#define _quadtree_h_
+
+#ifndef FLT
+#define FLT double
+#endif
+
+#ifndef ND
+#define ND 2
+#endif
+
+class quadtree {
+   private:
+      int maxvrtx;
+      FLT (*vrtx)[ND];
+      class quad *base;
+      class quad **indx;
+      int size;
+      int current;
+
+   public:
+      quadtree() : size(0) {};
+      quadtree(const quadtree& copy) : size(0) {*this = copy;}
+      quadtree& operator=(const quadtree& copy);
+      void change_vptr(FLT (*v)[ND]) { vrtx = v;}
+      void init(FLT (*v)[ND], int mxv, FLT x1, FLT y1, FLT x2, FLT y2);
+      void init(FLT x1, FLT y1, FLT x2, FLT y2);
+      void addpt(int v0, class quad *start = NULL);
+      FLT nearpt(int v0, int& pt) const;
+      FLT nearpt(FLT x, FLT y, int& pt) const;
+      void dltpt(int v0);
+      void movept(int from, int to);
+      void output(void);
+};
+
+class quad {
+   private:
+      friend class quadtree;
+      int num; 			// NUMBER OF POINTS
+      class quad *prnt;	// POINTER TO PARENT QUAD
+      int pind; 			// DAUGHTER NUMBER OF PARENT
+      FLT xmin, ymin;
+      FLT xmax, ymax;
+      union {
+         int node[4];    	// STORES EITHER NODES OR POINTER TO DAUGHTERS
+         class quad *dghtr[4];
+      };
+      
+   public:
+      quad() {}
+      quad(quad *p, int i, FLT x1, FLT y1, FLT x2, FLT y2) : 
+         num(0), prnt(p), pind(i), xmin(x1), ymin(y1), xmax(x2), ymax(y2) {}
+         
+};
+#endif
