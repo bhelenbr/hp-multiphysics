f988980161f845d5244f09d552f93be63fcd837e
helenbrk <helenbrk@346d1d51-7a3b-0410-9033-b9cd4ee96340>
2008-08-30 15:05:17 +0000

    Should be all working.

--- mathclass/main.cpp
+++ mathclass/main.cpp
@@ -1,13 +1,15 @@
-#include "mathclass.h"
+#include "symbolic_function.h"
 #include <input_map.h>
 
-int main() {
+#define NO_TESTING
+
+int main (int argc, char *argv[]) {
+
+#ifdef TESTING
 	input_map inmap;
 	std::string prefix;
 	symbolic_function<1> f;
-	symbolic_function<2> g;
-	symbolic_function<2> g1;
-    double x = 1.0;
+	symbolic_function<2> g,g1,g3;
 	blitz::TinyVector<double,2>  x2d(1.0, 0.0);
 		
 	inmap["Re"] = "100.0";
@@ -15,6 +17,7 @@ int main() {
 	inmap["E1"] = "exp(-x/l1*Re)";
 	inmap["E2"] = "sin(-x0/l1*Re+x1)";
 	inmap["E3"] = "x0*E2";
+	inmap["dhdx0"] = "2*h*ke^2*x0*exp(-ke^2*x0^2)*cos(k*x0-w*t)-h*(1-exp(-ke^2*x0^2))*sin(k*x0-w*t)*k-2*s*(x0-ex0)/denom^2*exp(-(x0-ex0)^2/denom^2)*(1+(2*h*ke^2*x0*exp(-ke^2*x0^2)*cos(k*x0-w*t)-h*(1-exp(-ke^2*x0^2))*sin(k*x0-w*t)*k)^2)^(1/2)-s*(1-exp(-(x0-ex0)^2/denom^2))/(1+(2*h*ke^2*x0*exp(-ke^2*x0^2)*cos(k*x0-w*t)-h*(1-exp(-ke^2*x0^2))*sin(k*x0-w*t)*k)^2)^(1/2)*(2*h*ke^2*x0*exp(-ke^2*x0^2)*cos(k*x0-w*t)-h*(1-exp(-ke^2*x0^2))*sin(k*x0-w*t)*k)*(2*h*ke^2*exp(-ke^2*x0^2)*cos(k*x0-w*t)-4*h*ke^4*x0^2*exp(-ke^2*x0^2)*cos(k*x0-w*t)-4*h*ke^2*x0*exp(-ke^2*x0^2)*sin(k*x0-w*t)*k-h*(1-exp(-ke^2*x0^2))*cos(k*x0-w*t)*k^2)";
 
 	std::cout << inmap << std::endl;
 
@@ -29,9 +32,30 @@ int main() {
 	g1.init(inmap,"E3");
 	std::cout << g1.Eval(x2d) << std::endl;
 	
-
-//	g1.copy_consts(g);
+	symbolic_function<2> g2(g1);
 	
+	std::cout << g2.Eval(x2d) << std::endl;
+	
+	g3.init(inmap,"dhdx0");
+	std::cout << g3.Eval(x2d,1.0) << std::endl;
+	
+#else
+    input_map mymap;
+	  
+    mymap.input(argv[1]);
+	
+	symbolic_function<3> f;
+	f.init(mymap,"formula");
+	
+	blitz::TinyVector<double,3> x;
+	mymap.get("position",x.data(),3);
+	
+	double t;
+	mymap.getwdefault("time",t,0.0);
+	
+	std::cout << f.Eval(x) << std::endl;
+	
+#endif
 
 	return(0);
 }
---
+++ mathclass/symbolic_function.h
@@ -0,0 +1,254 @@
+#ifndef _symbolic_function_h_
+#define _symbolic_function_h_
+
+#include <muParser.h>
+#include <blitz/array.h>
+#include <input_map.h>
+#include <math.h>
+
+template<int N> class symbolic_function {
+	protected:
+		mu::Parser p;
+		blitz::TinyVector<double,N+1> x; 
+		int nchildren;
+		blitz::Array<symbolic_function<N> *, 1> children;
+		blitz::Array<double,1> child_values;
+      
+	public:		
+	symbolic_function() {
+		std::ostringstream varname;
+		
+		p.DefineFun("erf", erf, false);
+		p.DefineFun("erfc", erfc, false);
+		
+		if (N > 1) {
+			for (int n=0;n<N;++n) {
+				varname.str("");
+				varname << "x" << n << std::flush;
+				p.DefineVar(varname.str(), &x(n));
+				varname.clear();
+			}
+		}
+		else if (N == 1) {
+			p.DefineVar("x", &x(0));
+		}
+      p.DefineVar("t",&x(N));
+	}
+	
+	symbolic_function(const symbolic_function& tgt) : p(tgt.p), nchildren(tgt.nchildren) {
+		std::ostringstream varname;
+
+		/* Reassociate Variables */
+		if (N > 1) {
+			for (int n=0;n<N;++n) {
+				varname.str("");
+				varname << "x" << n << std::flush;
+				p.DefineVar(varname.str(), &x(n));
+				varname.clear();
+			}
+		}
+		else if (N == 1) {
+			p.DefineVar("x", &x(0));
+		}
+      	p.DefineVar("t",&x(N));
+      	
+      	children.resize(nchildren);
+      	child_values.resize(nchildren);
+      	
+
+		mu::varmap_type variables = p.GetUsedVar();
+
+		/* Reassociate Children */
+		nchildren = 0;
+		for (mu::varmap_type::const_iterator item = variables.begin(); item!=variables.end(); ++item) {
+			if (N > 1) {
+				for (int n=0;n<N;++n) {
+					varname.str("");
+					varname << "x" << n << std::flush;
+					if (item->first == varname.str()) goto NEXT;
+					varname.clear();
+				}
+			}
+			else {
+				if (item->first == "x") goto NEXT;
+			}
+			if (item->first == "t") goto NEXT;
+			
+			/* This is a child variable */
+			children(nchildren) = new symbolic_function<N>(*tgt.children(nchildren));
+			p.DefineVar(item->first, &child_values(nchildren));
+			++nchildren;
+			NEXT: continue;
+		 }
+	}
+	
+	void init(input_map& input, std::string idprefix);
+		
+	double Eval(blitz::TinyVector<double,N> xin, double t = 0.0) {
+ 		double rslt;
+		for (int n = 0; n < N; ++n)
+			x(n) = xin(n);
+		
+		x(N) = t;
+
+		try {
+			for (int n=0;n<nchildren;++n) {
+				child_values(n) = children(n)->Eval(xin,t);
+			}
+			rslt = p.Eval();
+		}
+		catch (mu::Parser::exception_type &e) {
+			std::cout << "Message:  " << e.GetMsg() << std::endl;
+			std::cout << "Formula:  " << e.GetExpr() << std::endl;
+			std::cout << "Token:    " << e.GetToken() << std::endl;
+			std::cout << "Position: " << e.GetPos() << std::endl;
+			std::cout << "Errc:     " << e.GetCode() << std::endl;
+		}
+		return(rslt);
+	}
+	
+	/* Special case for one-spatial variable function */
+	double Eval(double xin, double t) {
+		double rslt;
+
+		x(0) = xin;
+		x(N) = t;
+		try {
+			for (int n=0;n<nchildren;++n) {
+				child_values(n) = children(n)->Eval(xin,t);
+			}
+			rslt = p.Eval();
+		}
+		catch (mu::Parser::exception_type &e) {
+			std::cout << "Message:  " << e.GetMsg() << std::endl;
+			std::cout << "Formula:  " << e.GetExpr() << std::endl;
+			std::cout << "Token:    " << e.GetToken() << std::endl;
+			std::cout << "Position: " << e.GetPos() << std::endl;
+			std::cout << "Errc:     " << e.GetCode() << std::endl;
+		}
+		return(rslt);
+	}
+
+	/* Special case for zero-variable functions */
+	double Eval(double t) {
+		double rslt;
+
+		x(0) = t;
+		try {
+			for (int n=0;n<nchildren;++n) {
+				child_values(n) = children(n)->Eval(t);
+			}
+			rslt = p.Eval();
+		}
+		catch (mu::Parser::exception_type &e) {
+			std::cout << "Message:  " << e.GetMsg() << std::endl;
+			std::cout << "Formula:  " << e.GetExpr() << std::endl;
+			std::cout << "Token:    " << e.GetToken() << std::endl;
+			std::cout << "Position: " << e.GetPos() << std::endl;
+			std::cout << "Errc:     " << e.GetCode() << std::endl;
+		}
+		return(rslt);
+	}
+		
+};	
+
+template<int N> void symbolic_function<N>::init(input_map& input, std::string idprefix) {
+	std::ostringstream conststring, varname;
+	std::istringstream constname;
+	std::string buffer,name,keyword;
+	double value;
+	
+	/* LOAD CONSTANTS IN FORMULA */
+	mu::Parser ptemp;
+	ptemp.DefineFun("erf", erf, false);
+	ptemp.DefineFun("erfc", erfc, false);
+  
+	if (!input.getline(idprefix,buffer)) {
+	   std::cout << "couldn't find expression" << idprefix << '\n';
+	}
+	ptemp.SetExpr(buffer);
+	mu::varmap_type variables = ptemp.GetUsedVar();
+
+
+
+	/* Find out how many children are needed */
+	nchildren = 0;
+	for (mu::varmap_type::const_iterator item = variables.begin(); item!=variables.end(); ++item) {
+		if (N > 1) {
+			for (int n=0;n<N;++n) {
+				varname.str("");
+				varname << "x" << n << std::flush;
+				if (item->first == varname.str()) goto NEXT;
+				varname.clear();
+			}
+		}
+		else {
+			if (item->first == "x") goto NEXT;
+		}
+		if (item->first == "t") goto NEXT;
+		
+		if (!input.get(item->first,value)) {
+			/* Check if it is not there or if it is also defined as a formula */
+			std::map<std::string,std::string>::const_iterator mi;
+    		mi = input.find(item->first);
+			if (mi != input.end()) {
+				++nchildren;
+				goto NEXT;
+			}
+			else {
+				std::cout << "couldn't find expression " << item->first << '\n';
+				exit(1);
+			}
+		}
+		
+		NEXT: continue;
+	 }
+
+	children.resize(nchildren);
+	child_values.resize(nchildren);
+	/* REPEAT EXCEPT THIS TIME ALLOCATE CHILDREN */
+	nchildren = 0;
+	for (mu::varmap_type::const_iterator item = variables.begin(); item!=variables.end(); ++item) {
+		if (N > 1) {
+			for (int n=0;n<N;++n) {
+				varname.str("");
+				varname << "x" << n << std::flush;
+				if (item->first == varname.str()) goto NEXT1;
+				varname.clear();
+			}
+		}
+		else {
+			if (item->first == "x") goto NEXT1;
+		}
+		if (item->first == "t") goto NEXT1;
+		
+		if (!input.get(item->first,value)) {
+			/* Check if it is not there or if it is also defined as a formula */
+			std::map<std::string,std::string>::const_iterator mi;
+    		mi = input.find(item->first);
+			if (mi != input.end()) {
+				/* It is there */
+				children(nchildren) = new symbolic_function<N>;
+				children(nchildren)->init(input,item->first);
+				p.DefineVar(item->first, &child_values(nchildren));
+				++nchildren;
+				goto NEXT1;
+			}
+			else {
+				std::cout << "couldn't find expression " << item->first << '\n';
+				exit(1);
+			}
+		}
+		p.DefineConst(item->first,value);
+		
+		NEXT1: continue;
+	 }
+	 p.SetExpr(buffer);
+	
+	 return;
+ }
+	 
+	 
+
+	
+#endif
