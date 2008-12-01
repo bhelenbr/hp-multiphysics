65acafa441e3344c322e503608f9bbcaeb51f115
helenbrk <helenbrk@346d1d51-7a3b-0410-9033-b9cd4ee96340>
2008-12-01 14:12:56 +0000

    Added ability to count operations.

--- tri_basis/main.cpp
+++ tri_basis/main.cpp
@@ -7,6 +7,9 @@
 #define P 4
 #define GP P+2
 
+#define OP_COUNT
+#include <CHUD/CHUD.h>
+
 int main (int argc, const char * argv[]) {
     clock_t cpu_time;
     
@@ -19,12 +22,25 @@ int main (int argc, const char * argv[]) {
     double u[GP][GP];
     double dx[GP][GP];
     double dy[GP][GP];
-    
-    for (int i = 0; i < 40000; ++i) {
+	
+#ifdef OP_COUNT
+	chudInitialize(); 
+	chudAcquireRemoteAccess(); 
+	chudStartRemotePerfMonitor("fpucount"); 
+#endif
+	
+    for (int i = 0; i < 10000; ++i) {
         basis::tri(0).proj(uht,u[0],dx[0],dy[0],GP);
       	basis::tri(0).intgrtrs(uht,dx[0],dy[0],GP); 
         basis::tri(0).intgrt(uht,dx[0],GP); 
     }
+	
+	
+#ifdef OP_COUNT
+	chudStopRemotePerfMonitor(); 
+	chudReleaseRemoteAccess();
+#endif
+		
     cpu_time = clock();
     printf("#that took %ld cpu time\n",cpu_time);        
 
