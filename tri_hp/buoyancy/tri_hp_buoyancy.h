5b4c3fde997d9206e37772664a6a1fb41f37061f
brazelmj <brazelmj@346d1d51-7a3b-0410-9033-b9cd4ee96340>
2010-04-14 21:21:33 +0000

    added length to tri_hp_buoyancy.h

--- tri_hp/buoyancy/tri_hp_buoyancy.h
+++ tri_hp/buoyancy/tri_hp_buoyancy.h
@@ -30,7 +30,8 @@ class tri_hp_buoyancy : public tri_hp_ins {
 
 		void init(input_map& input, void *gin); 
 		void init(const multigrid_interface& in, init_purpose why=duplicate, FLT sizereduce1d=1.0);
-
+		
+		void length();
 		void setup_preconditioner();
 		void element_rsdl(int tind, int stage, Array<TinyVector<FLT,MXTM>,1> &uhat,Array<TinyVector<FLT,MXTM>,1> &lf_re,Array<TinyVector<FLT,MXTM>,1> &lf_im);
 		void calculate_unsteady_sources();
