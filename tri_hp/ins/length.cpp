3f2b405bd5b88886a3c5ebdc8a89d14623463531
helenbrk <helenbrk@346d1d51-7a3b-0410-9033-b9cd4ee96340>
2010-05-06 18:10:23 +0000

    only allow length scale changes with scale factor of 2.

--- tri_hp/ins/length.cpp
+++ tri_hp/ins/length.cpp
@@ -23,7 +23,7 @@ void tri_hp_ins::length() {
 	Array<TinyMatrix<FLT,MXGP,MXGP>,1> u(NV),ul(NV);
 	Array<TinyMatrix<FLT,MXGP,MXGP>,2> du(NV,ND), dul(NV,ND);
 	
-	return;  // TEMPORARY To simply maintain mesh quality
+	// return; TEMPORARY To simply maintain mesh quality
 		
 	int sm = basis::tri(log2p)->sm();
 	int lgpx = basis::tri(log2p)->gpx();
