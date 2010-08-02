ac1ced6e29041508168c32730a77a3cfaa4f960f
bhelenbr <bhelenbr@346d1d51-7a3b-0410-9033-b9cd4ee96340>
2010-08-02 09:50:05 +0000

    Added partitioning script to project.

---
+++ tri_mesh/partition.bash
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+MESH=$(grep ^[^#]*esh $1 | cut -d: -f 2)
+FTYP=$(grep ^[^#]*iletype $1 | cut -d: -f 2)
+BLKS=$(grep ^[^#]*block $1 | cut -d: -f 2)
+echo "Partitioning ${MESH} of type ${FTYP} into ${BLKS} parts"
+
+cat $1 | grep -v nblock | grep -v filetype | grep -v b0_mesh > partition.inpt
+
+#echo "text \c"
+# or
+#echo -n "text "
+
+echo "filetype: 3" >> partition.inpt
+echo -n "nblock: " >> partition.inpt
+let p=0
+while [ $p -lt $BLKS ]; do
+	echo -n "1 " >> partition.inpt
+	let p=$p+1
+done
+echo "" >> partition.inpt
+
+let p=1
+while [ $p -lt $BLKS ]; do
+	grep b0 $1 | grep -v filetype | grep -v mesh | sed "s/b0/b$p/g" >> partition.inpt
+	let p=$p+1
+done
+
+echo $FTYP
+echo $MESH
+echo $BLKS
+	
+if [ -n "$FTYP" ]; 
+then
+        echo "tri_mesh -i $FTYP -p $MESH $BLKS | grep -v '#creating' >> partition.inpt"
+	tri_mesh -i $FTYP -p $MESH $BLKS | grep -v '#creating' >> partition.inpt
+else
+        echo "tri_mesh -p $MESH $BLKS | grep -v '#creating' >> partition.inpt"
+	tri_mesh -p $MESH $BLKS | grep -v '#creating' >> partition.inpt
+fi
+
+
+# Remove unnecessary communication endpoints (ones only on 2 blocks)
+RMV=$(grep '_v' partition.inpt | grep ': comm' | cut -d _ -f2 | sort | uniq -c | sort | grep '  2 v' | cut -d \  -f 5)
+
+cp partition.inpt temp.inpt
+for line in ${RMV}; do
+	echo ${line}
+	grep -v ${line} temp.inpt > temp1.inpt
+	mv temp1.inpt temp.inpt
+done
+mv temp.inpt partition.inpt
+sed 's/partition$/comm/g' partition.inpt > comm.inpt
