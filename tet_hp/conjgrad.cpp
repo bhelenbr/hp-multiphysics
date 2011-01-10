92e86b9dcc99b75e9e69128bd1ec070da53e51ba
guest <guest@346d1d51-7a3b-0410-9033-b9cd4ee96340>
2011-01-10 21:26:45 +0000

    added to project

---
+++ tet_hp/conjgrad.cpp
@@ -0,0 +1,221 @@
+/*
+ *  conjgrad.cpp
+ *  tet_hp
+ *
+ *  Created by michael brazell on 1/10/11.
+ *  Copyright 2011 Clarkson University. All rights reserved.
+ *
+ */
+
+
+#include "tet_hp.h"
+#include "hp_boundary.h"
+#include <myblas.h>
+
+void tet_hp::conjugate_gradient() {
+	
+	rsdl();
+	
+	FLT omega = 1.0;
+	FLT alpha,beta;
+	int em = basis::tet(log2p).em;
+	int fm = basis::tet(log2p).fm;
+	int im = basis::tet(log2p).im;
+	
+	struct vefi p;
+	p.v.resize(maxvst,NV);
+	p.e.resize(maxvst,em0,NV);
+	p.f.resize(maxvst,fm0,NV);
+	p.i.resize(maxvst,im0,NV);
+	
+	struct vefi z;
+	z.v.resize(maxvst,NV);
+	z.e.resize(maxvst,em0,NV);
+	z.f.resize(maxvst,fm0,NV);
+	z.i.resize(maxvst,im0,NV);
+	
+	/* APPLY VERTEX DIRICHLET B.C.'S */
+	for(int i=0;i<nfbd;++i)
+		hp_fbdry(i)->vdirichlet();
+	
+	for(int i=0;i<nebd;++i)
+		hp_ebdry(i)->vdirichlet3d();        
+	
+	for(int i=0;i<nvbd;++i)
+		hp_vbdry(i)->vdirichlet3d();
+	
+	/* APPLY EDGE DIRICHLET B.C.'S */
+    for(int i=0;i<nfbd;++i)
+        hp_fbdry(i)->edirichlet();	
+	
+	for (int i=0;i<nebd;++i) 
+		hp_ebdry(i)->edirichlet3d();
+	
+	/* APPLY FACE DIRICHLET B.C.'S */
+	for(int i=0;i<nfbd;++i)
+		hp_fbdry(i)->fdirichlet();
+	
+	z = gbl->res;
+	
+	jacobi_relaxation();
+	//tri_hp::minvrt();
+	
+	p = gbl->res;
+	
+	struct vefi Ap;
+	Ap.v.resize(maxvst,NV);
+	Ap.e.resize(maxvst,em0,NV);
+	Ap.f.resize(maxvst,fm0,NV);
+	Ap.i.resize(maxvst,im0,NV);
+	
+	FLT rtrold,rtrnew;
+	
+	inner_product(rtrold,z,gbl->res);
+	
+	for(int i = 0; i < 300; ++i) {
+		Ap = p;	
+		matrix_multiply(Ap);
+		FLT ptAp;
+		inner_product(ptAp,p,Ap);
+		
+		alpha = rtrold/ptAp;
+		
+		ug.v = ug.v - alpha*p.v;
+		if(em) ug.e = ug.e - alpha*p.e;
+		if(fm) ug.f = ug.f - alpha*p.f;
+		if(im) ug.i = ug.i - alpha*p.i;
+		
+		//		gbl->res.v = gbl->res.v - alpha*Ap.v;
+		//		if(sm) gbl->res.s = gbl->res.s - alpha*Ap.s;
+		//		if(im) gbl->res.i = gbl->res.i - alpha*Ap.i;
+		
+		rsdl();		
+		
+		/* APPLY VERTEX DIRICHLET B.C.'S */
+		for(int i=0;i<nfbd;++i)
+			hp_fbdry(i)->vdirichlet();
+		
+		for(int i=0;i<nebd;++i)
+			hp_ebdry(i)->vdirichlet3d();        
+		
+		for(int i=0;i<nvbd;++i)
+			hp_vbdry(i)->vdirichlet3d();
+		
+		/* APPLY EDGE DIRICHLET B.C.'S */
+		for(int i=0;i<nfbd;++i)
+			hp_fbdry(i)->edirichlet();	
+		
+		for (int i=0;i<nebd;++i) 
+			hp_ebdry(i)->edirichlet3d();
+		
+		/* APPLY FACE DIRICHLET B.C.'S */
+		for(int i=0;i<nfbd;++i)
+			hp_fbdry(i)->fdirichlet();
+		
+		z = gbl->res;
+		
+		jacobi_relaxation();
+		//tri_hp::minvrt();
+		
+		inner_product(rtrnew,z,gbl->res);
+		
+		beta = rtrnew/rtrold;
+		
+		p.v = gbl->res.v + beta*p.v;
+		if(em) p.e = gbl->res.e + beta*p.e;
+		if(fm) p.f = gbl->res.f + beta*p.f;
+		if(im) p.i = gbl->res.i + beta*p.i;
+		
+		rtrold = rtrnew;
+		FLT resmax = maxres();
+		cout <<  i << ' ' << rtrold << ' ' << resmax << endl;
+		if(resmax < 1.0e-10) break;
+		
+	}
+	exit(4);
+	
+	return;
+	
+}
+
+
+
+void tet_hp::matrix_multiply(struct vefi& vec) {
+	
+#ifdef BZ_DEBUG
+	const FLT eps_r = 0.0e-6, eps_a = 1.0e-6;  /*<< constants for debugging jacobians */
+#else
+	const FLT eps_r = 1.0e-6, eps_a = 1.0e-10;  /*<< constants for accurate numerical determination of jacobians */
+#endif
+	
+	int em = basis::tet(log2p).em;
+	int fm = basis::tet(log2p).fm;
+	int im = basis::tet(log2p).im;
+	
+	struct vefi temp;
+	temp.v.resize(maxvst,NV);
+	temp.e.resize(maxvst,em0,NV);
+	temp.f.resize(maxvst,fm0,NV);
+	temp.i.resize(maxvst,im0,NV);
+	
+	FLT dw = 0.0;
+ 	for(int i=0;i<npnt;++i)
+		for(int n=0;n<NV;++n)
+			dw = MAX(dw,fabs(ug.v(i,n)));
+	
+	dw = dw*eps_r;
+	dw = dw+eps_a;
+	
+	rsdl();// rsdl may already be called
+	
+	temp = gbl->res;
+	
+	ug.v = ug.v + dw*vec.v;
+	if(em) ug.e = ug.e + dw*vec.e;
+	if(fm) ug.f = ug.f + dw*vec.f;
+	if(im) ug.i = ug.i + dw*vec.i;
+	
+	rsdl();
+	
+	ug.v = ug.v - dw*vec.v;
+	if(em) ug.e = ug.e - dw*vec.e;
+	if(fm) ug.f = ug.f - dw*vec.f;
+	if(im) ug.i = ug.i - dw*vec.i;
+	
+	vec.v = (gbl->res.v - temp.v)/dw;
+	if(em) vec.e = (gbl->res.e - temp.e)/dw;
+	if(fm) vec.f = (gbl->res.f - temp.f)/dw;
+	if(im) vec.i = (gbl->res.i - temp.i)/dw;
+	
+	gbl->res = temp;	
+	
+	return;
+	
+}
+
+
+void tet_hp::inner_product(FLT &alpha, struct vefi vec1,struct vefi vec2 ) {
+	
+	alpha = 0.0;
+	
+	for(int i=0; i < npnt; ++i) 
+		for(int n=0;n<NV;++n)
+			alpha += vec1.v(i,n)*vec2.v(i,n);
+	
+	for(int i=0; i < nseg; ++i) 
+		for(int m=0;m<basis::tet(log2p).em;++m)
+			for(int n=0;n<NV;++n)
+				alpha += vec1.e(i,m,n)*vec2.e(i,m,n);
+		
+	for(int i=0; i < ntri; ++i) 
+		for(int m=0;m<basis::tet(log2p).fm;++m)
+			for(int n=0;n<NV;++n)
+				alpha += vec1.f(i,m,n)*vec2.f(i,m,n);
+	
+	for(int i=0; i < ntet; ++i) 
+		for(int m=0;m<basis::tet(log2p).im;++m)
+			for(int n=0;n<NV;++n)
+				alpha += vec1.i(i,m,n)*vec2.i(i,m,n);
+	
+	return;	
+}
---
+++ tet_hp/jacobi.cpp
@@ -0,0 +1,131 @@
+/*
+ *  jacobi.cpp
+ *  tet_hp
+ *
+ *  Created by michael brazell on 1/10/11.
+ *  Copyright 2011 Clarkson University. All rights reserved.
+ *
+ */
+
+#include "tet_hp.h"
+#include "hp_boundary.h"
+#include <myblas.h>
+
+void tet_hp::jacobi_relaxation() {
+	int em = basis::tet(log2p).em;
+	int fm = basis::tet(log2p).fm;
+	int im = basis::tet(log2p).im;
+	
+	/* Jacobi's relaxation */
+	FLT omega = 1.0;
+	gbl->res.v(Range(0,npnt-1),Range::all()) = omega*gbl->res.v(Range(0,npnt-1),Range::all())/gbl->jacob_diag.v(Range(0,npnt-1),Range::all());
+	if(em) gbl->res.e(Range(0,nseg-1),Range(0,em-1),Range::all()) = omega*gbl->res.e(Range(0,nseg-1),Range(0,em-1),Range::all())/gbl->jacob_diag.e(Range(0,nseg-1),Range(0,em-1),Range::all());
+	if(fm) gbl->res.f(Range(0,ntri-1),Range(0,fm-1),Range::all()) = omega*gbl->res.f(Range(0,ntri-1),Range(0,fm-1),Range::all())/gbl->jacob_diag.f(Range(0,ntri-1),Range(0,fm-1),Range::all());
+	if(im) gbl->res.i(Range(0,ntet-1),Range(0,im-1),Range::all()) = omega*gbl->res.i(Range(0,ntet-1),Range(0,im-1),Range::all())/gbl->jacob_diag.i(Range(0,ntet-1),Range(0,im-1),Range::all());
+	
+	
+	/* APPLY VERTEX DIRICHLET B.C.'S */
+	for(int i=0;i<nfbd;++i)
+		hp_fbdry(i)->vdirichlet();
+	
+	for(int i=0;i<nebd;++i)
+		hp_ebdry(i)->vdirichlet3d();        
+	
+	for(int i=0;i<nvbd;++i)
+		hp_vbdry(i)->vdirichlet3d();
+	
+	/* APPLY EDGE DIRICHLET B.C.'S */
+    for(int i=0;i<nfbd;++i)
+        hp_fbdry(i)->edirichlet();	
+	
+	for (int i=0;i<nebd;++i) 
+		hp_ebdry(i)->edirichlet3d();
+
+	/* APPLY FACE DIRICHLET B.C.'S */
+	for(int i=0;i<nfbd;++i)
+		hp_fbdry(i)->fdirichlet();
+	
+	
+	return;
+}
+
+void tet_hp::jacobian_diagonal() {
+	
+	Array<TinyVector<FLT,MXTM>,1> Rbar(NV),lf_re(NV),lf_im(NV);
+	Array<FLT,1> dw(NV);
+#ifdef BZ_DEBUG
+	const FLT eps_r = 0.0e-6, eps_a = 1.0e-6;  /*<< constants for debugging jacobians */
+#else
+	const FLT eps_r = 1.0e-6, eps_a = 1.0e-10;  /*<< constants for accurate numerical determination of jacobians */
+#endif
+	
+	int em = basis::tet(log2p).em;
+	int fm = basis::tet(log2p).fm;
+	int im = basis::tet(log2p).im;
+	int tm = basis::tet(log2p).tm;
+	
+	int dof = tm*NV;
+	Array<FLT,1> Kdiag(dof);
+	
+	gbl->jacob_diag.v = 0.0;
+	gbl->jacob_diag.e = 0.0;
+	gbl->jacob_diag.f = 0.0;
+	gbl->jacob_diag.i = 0.0;
+	
+	
+	/* Calculate diagonal stiffness matrix terms */
+	for(int tind = 0; tind < ntet; ++tind) {	
+		
+		ugtouht(tind);
+		
+		dw = 0.0;
+		for(int i=0;i<4;++i)
+			for(int n=0;n<NV;++n)
+				dw = dw + fabs(uht(n)(i));
+		
+		dw = dw*eps_r;
+		dw = dw+eps_a;
+		
+		element_rsdl(tind,0,uht,lf_re,lf_im);
+		
+		for(int i=0;i<tm;++i) 
+			for(int n=0;n<NV;++n) 
+				Rbar(n)(i)=lf_re(n)(i)+lf_im(n)(i);		
+		
+		int ind = 0;
+		for(int i = 0; i < tm; ++i){
+			for(int n = 0; n < NV; ++n){
+				uht(n)(i) += dw(n);
+				
+				element_rsdl(tind,0,uht,lf_re,lf_im);
+				
+				Kdiag(ind++) = (lf_re(n)(i) +lf_im(n)(i) -Rbar(n)(i))/dw(n);
+				
+				uht(n)(i) -= dw(n);
+			}
+		}
+		
+		ind = 0;	
+		for (int i=0; i < 4; ++i) 
+			for(int n=0;n<NV;++n)
+				gbl->jacob_diag.v(tet(tind).pnt(i),n) += Kdiag(ind++);
+		
+		for (int i=0; i < 6; ++i) 
+			for(int m=0;m<em;++m)
+				for(int n=0;n<NV;++n)
+					gbl->jacob_diag.e(tet(tind).seg(i),m,n) += Kdiag(ind++);
+		
+		for (int i=0; i < 4; ++i) 
+			for(int m=0;m<fm;++m)
+				for(int n=0;n<NV;++n)
+					gbl->jacob_diag.f(tet(tind).tri(i),m,n) += Kdiag(ind++);
+		
+		for(int m=0;m<im;++m)
+			for(int n=0;n<NV;++n)
+				gbl->jacob_diag.i(tind,m,n) += Kdiag(ind++);			
+		
+		
+	}
+	
+	return;
+}
