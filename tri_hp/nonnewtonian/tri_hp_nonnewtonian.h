7d0a3bffa32ddb979a7a4141b9eda1552001b2e7
bhelenbr <bhelenbr@346d1d51-7a3b-0410-9033-b9cd4ee96340>
2011-07-30 05:53:39 +0000

    fixed syntax error

--- tri_hp/nonnewtonian/tri_hp_nonnewtonian.h
+++ tri_hp/nonnewtonian/tri_hp_nonnewtonian.h
@@ -31,6 +31,6 @@ class tri_hp_nonnewtonian : public tri_hp_ins {
 		void init(input_map& input, void *gin); 
 		void init(const multigrid_interface& in, init_purpose why=duplicate, FLT sizereduce1d=1.0);
 		void setup_preconditioner();
-		voi d element_rsdl(int tind, int stage, Array<TinyVector<FLT,MXTM>,1> &uhat,Array<TinyVector<FLT,MXTM>,1> &lf_re,Array<TinyVector<FLT,MXTM>,1> &lf_im);
+		void element_rsdl(int tind, int stage, Array<TinyVector<FLT,MXTM>,1> &uhat,Array<TinyVector<FLT,MXTM>,1> &lf_re,Array<TinyVector<FLT,MXTM>,1> &lf_im);
 };
 #endif
