aefdf6a1330a1afc46fe51a449cc207b284431f4
guest <guest@346d1d51-7a3b-0410-9033-b9cd4ee96340>
2011-08-08 13:58:17 +0000

    Added new boundary condition soi_src

--- tri_hp/cd/getnewibc.cpp
+++ tri_hp/cd/getnewibc.cpp
@@ -1,16 +1,19 @@
 /*
- *	getnewibc_ins.cpp
- *	tri_hp
+ *  getnewibc_ins.cpp
+ *  tri_hp
  *
- *	Created by Brian Helenbrook on 2/6/06.
- *	Copyright 2006 __MyCompanyName__. All rights reserved.
+ *  Created by Brian Helenbrook on 2/6/06.
+ *  Copyright 2006 __MyCompanyName__. All rights reserved.
  *
  */
 
 #include "tri_hp_cd.h"
 #include <cstring>
 #include <math.h>
-
+#include <fstream>
+#include <iostream>
+//#include <time.h>
+using std::ifstream;
 namespace ibc_cd {
 	
 #ifdef CONVDIFF
@@ -34,9 +37,9 @@ namespace ibc_cd {
 		switch(n) {
 			case((0+CASE)%3):
 				return(axx*sin(xx)/(axx*axx +nux*nux)
-						 +nux*cos(xx)/(axx*axx +nux*nux)
-						 +(nux > 0.0 ? blayer*exp(axx/nux*xx) : 0.0)
-						 +startup*((sin(xx) +sin(100*xx))*(sin(yx)+sin(100*yx))));
+					   +nux*cos(xx)/(axx*axx +nux*nux)
+					   +(nux > 0.0 ? blayer*exp(axx/nux*xx) : 0.0)
+					   +startup*((sin(xx) +sin(100*xx))*(sin(yx)+sin(100*yx))));
 			case((1+CASE)%3):
 				return(startup*((sin(xx) +sin(100*xx))*(sin(yx)+sin(100*yx))));
 			case((2+CASE)%3):
@@ -54,8 +57,8 @@ namespace ibc_cd {
 		switch(n) {
 			case((0+CASE)%3):
 				return(axx*cos(xx)/(axx*axx +nux*nux) 
-						 -nuext*sin(x)/(axx*axx +nux*nux)
-						 +(nux > 0.0 ? blayer*axx/nux*exp(axx/nux*xx) : 0.0));
+					   -nuext*sin(x)/(axx*axx +nux*nux)
+					   +(nux > 0.0 ? blayer*axx/nux*exp(axx/nux*xx) : 0.0));
 			case(!CASE):
 				return(0.0);
 		}
@@ -65,129 +68,202 @@ namespace ibc_cd {
 #endif
 	
 	class zero_src : public init_bdry_cndtn {
-		public:
-			FLT f(int n, TinyVector<FLT,tri_mesh::ND> x,FLT time) {
-				return(0.0);
-			}
-			void input(input_map &inmap,std::string idnty) {}
+	public:
+		FLT f(int n, TinyVector<FLT,tri_mesh::ND> x,FLT time) {
+			return(0.0);
+		}
+		void input(input_map &inmap,std::string idnty) {}
 	};
 	
 	
 	class power_src : public init_bdry_cndtn {
-		private:
-			Array<FLT,1> c;
-			Array<FLT,2> a;
-			Array<FLT,2> spd;
-		public:
-			FLT f(int n, TinyVector<FLT,tri_mesh::ND> x, FLT time) {
-				return(c(n)*pow(x(0)-spd(n,0)*time,a(n,0))*pow(x(1)-spd(n,1)*time,a(n,1)));
-			}
-			void input(input_map &inmap,std::string idnty) {
-				std::string keyword,val;
-				std::istringstream data;
-				std::ostringstream nstr;
-				int nvar;
-				
-				keyword = idnty +"_nvariable";
-				
-				if (!inmap.get(keyword,nvar))
-					inmap.getwdefault("nvariable",nvar,1);
-				
-				c.resize(nvar);
-				a.resize(nvar,2);
-				spd.resize(nvar,2);
-				
-				for(int n=0;n<nvar;++n) {
-					nstr.str("");
-					nstr << idnty << "_src_coeff" << n << std::flush;
-					if (!inmap.get(nstr.str(),c(n))) {
-						inmap.getwdefault("src_coeff",c(n),0.0);
-					}
-					
-					nstr.str("");
-					nstr << idnty << "_src_powers" << n << std::flush;
-					if (!inmap.getline(nstr.str(),val)) {
-						nstr.str("");
-						nstr << "src_powers" << n << std::flush;
-						inmap.getlinewdefault(nstr.str(),val,"0.0 0.0");
-					}								
-					data.str(val);
-					data >> a(n,0) >> a(n,1);	
-					data.clear(); 
-					
-					nstr.str("");
-					nstr << idnty << "_src_speeds" << n << std::flush;
-					if (!inmap.getline(nstr.str(),val)) {
-						nstr.str("");
-						nstr << "src_speeds" << n << std::flush;
-						inmap.getlinewdefault(nstr.str(),val,"0.0 0.0");
-					}								
-					data.str(val);
-					data >> spd(n,0) >> spd(n,1);	
-					data.clear(); 
+	private:
+		Array<FLT,1> c;
+		Array<FLT,2> a;
+		Array<FLT,2> spd;
+	public:
+		FLT f(int n, TinyVector<FLT,tri_mesh::ND> x, FLT time) {
+			return(c(n)*pow(x(0)-spd(n,0)*time,a(n,0))*pow(x(1)-spd(n,1)*time,a(n,1)));
+		}
+		void input(input_map &inmap,std::string idnty) {
+			std::string keyword,val;
+			std::istringstream data;
+			std::ostringstream nstr;
+			int nvar;
+			
+			keyword = idnty +"_nvariable";
+			
+			if (!inmap.get(keyword,nvar))
+				inmap.getwdefault("nvariable",nvar,1);
+			
+			c.resize(nvar);
+			a.resize(nvar,2);
+			spd.resize(nvar,2);
+			
+			for(int n=0;n<nvar;++n) {
+				nstr.str("");
+				nstr << idnty << "_src_coeff" << n << std::flush;
+				if (!inmap.get(nstr.str(),c(n))) {
+					inmap.getwdefault("src_coeff",c(n),0.0);
 				}
+				
+				nstr.str("");
+				nstr << idnty << "_src_powers" << n << std::flush;
+				if (!inmap.getline(nstr.str(),val)) {
+					nstr.str("");
+					nstr << "src_powers" << n << std::flush;
+					inmap.getlinewdefault(nstr.str(),val,"0.0 0.0");
+				}                
+				data.str(val);
+				data >> a(n,0) >> a(n,1);  
+				data.clear(); 
+				
+				nstr.str("");
+				nstr << idnty << "_src_speeds" << n << std::flush;
+				if (!inmap.getline(nstr.str(),val)) {
+					nstr.str("");
+					nstr << "src_speeds" << n << std::flush;
+					inmap.getlinewdefault(nstr.str(),val,"0.0 0.0");
+				}                
+				data.str(val);
+				data >> spd(n,0) >> spd(n,1);  
+				data.clear(); 
 			}
+		}
 	};
 	
 	class soi_src : public init_bdry_cndtn {
 		tri_hp_cd &x;
+		ifstream infile;
 		
-		public:
-			double fq,wth,c; //	(frequency (fq), width (wth), power input (c))
-			int cycle,source_switch;
+	public:
+		double fq,wth,c,S; //	(frequency (fq), width (wth), power input (c))
+		int cycle,source_switch,icount;// R is the random key;icount is increment counters;L is the option of data loading
+		string binary_filename;
+		
+		FLT f(int n, TinyVector<FLT,tri_mesh::ND> xpt, FLT time) {
+			std::string keyword,val,inf,blockidnum;
 			
-			FLT f(int n, TinyVector<FLT,tri_mesh::ND> xpt, FLT time) {
-				//*x.gbl->log <<" I am here " << std::endl;
-				//*x.gbl->log << fq << ' ' << time << ' ' << source_switch << std::endl;
-				int current_cycle = static_cast<int>(fq*(time+1.0e-8));
-				if (current_cycle > cycle) {
-					// I'm on a new time step!
+			
+			//*x.gbl->log << fq << ' ' << time << ' ' << source_switch << std::endl;
+			int current_cycle = static_cast<int>(fq*(time+1.0e-8));
+			if (current_cycle > cycle) {
+				// I'm on a new time step!
+				
+				
+				/*Option to load data from a file*/
+				if (S==0.0){ 
+					
+					//Operation to be performed at the end of the file
+					if (infile.eof()) {
+						infile.close();
+						*x.gbl->log << "REACHED THE END OF THE FILE" << std::endl;
+						sim::abort(__LINE__,__FILE__,&std::cerr);
+					}
+					//making sure the file is open
+					if (!infile.good()) {
+						
+						*x.gbl->log << "Not in file "<< std::endl;
+						sim::abort(__LINE__,__FILE__,&std::cerr);
+						
+					}
+					else {
+						*x.gbl->log << "line in file =  " << icount << std::endl; 
+						infile >> source_switch;
+						
+						
+						
+						*x.gbl->log <<" Source_switch is = " << source_switch << std::endl;
+						if (source_switch==0){
+							*x.gbl->log << "#Switch is off this cycle\n"; 
+							
+						}
+						if (source_switch==1){
+							*x.gbl->log << "#Switch is on this cycle\n";
+							
+						}
+						
+					}
+					
+				}
+				
+				/*Switch between Random and Continuous*/
+				/*Continuous*/
+				if (S==1.0) 	{
+					source_switch=1;
+					*x.gbl->log << "#Switch is on this cycle\n";
+				}
+				/*Random*/
+				if (S==2.0){
+					//srand (time);
+					//srand(1);
+                 *x.gbl->log << "seed counter at" << time << std::endl; 
 					FLT random_number=rand() % 100;
 					if (random_number < 50) {
-						source_switch = 0;
-						*x.gbl->log << "#Switch is off this cycle\n" ;
-
-					} else {
 						source_switch = 1;
 						*x.gbl->log << "#Switch is on this cycle\n" ;
-
+						
+					} else {
+						source_switch =0;
+						*x.gbl->log << "#Switch is off this cycle\n" ;
+						
 					}
-					cycle = current_cycle;
-				}
-				FLT omegat = (2.*M_PI*fq*time+(1/2)*M_PI);
-				return(source_switch*c*(exp(cos(omegat)/wth))/(exp(1/wth)));
+				}   
+				
+				icount=icount++;
+				cycle = current_cycle;
 			}
-			soi_src(tri_hp_cd& xin) : x(xin), cycle(-1) {}
+			
+			FLT omegat = (2.*M_PI*fq*time+(1/2)*M_PI);
+			return(source_switch*c*(exp(cos(omegat)/wth))/(exp(1/wth))); 
+			
+		}
+		soi_src(tri_hp_cd& xin) : x(xin), cycle(-1),icount(0) {}
+		void input(input_map &blockdata,std::string idnty) {
+			std::string keyword,val;
+			std::istringstream data;
+			
+			
+			keyword = idnty+"_simopt";
+			blockdata.getwdefault(keyword,S,1.0);
+			
+			keyword = idnty+"_frequency";
+			blockdata.getwdefault(keyword,fq,0.595);
+			
+			keyword = idnty+"_width";
+			blockdata.getwdefault(keyword,wth,0.1);
+			
+			keyword = idnty+"_constant";
+			blockdata.getwdefault(keyword,c,0.89);
+			
+			
+			keyword = x.gbl->idprefix + "_binary_data";
+			blockdata.get(keyword,binary_filename);
+			infile.open(binary_filename.c_str());
+			
+		}	
 		
-			void input(input_map &blockdata,std::string idnty) {
-				std::string keyword,val;
-				std::istringstream data;
-				
-				keyword = idnty+"_frequency";
-				blockdata.getwdefault(keyword,fq,0.595);
-				
-				keyword = idnty+"_width";
-				blockdata.getwdefault(keyword,wth,0.1);
-				
-				keyword = idnty+"_constant";
-				blockdata.getwdefault(keyword,c,0.089);
-			}
+		
 	};
-
+	
+	
+	
+	
+	
 	class ibc_type {
-		public:
-			const static int ntypes = 3;
-			enum ids {zero,power,soi};
-			const static char names[ntypes][40];
-			static int getid(const char *nin) {
-				int i;
-				for(i=0;i<ntypes;++i) 
-					if (!strcmp(nin,names[i])) return(i);
-				return(-1);
-			}
+	public:
+		const static int ntypes = 3;
+		enum ids {zero,power,soi};
+		const static char names[ntypes][40];
+		static int getid(const char *nin) {
+			int i;
+			for(i=0;i<ntypes;++i) 
+				if (!strcmp(nin,names[i])) return(i);
+			return(-1);
+		}
 	};
 	const char ibc_type::names[ntypes][40] = {"zero","power","soi"};
-
+	
 }
 
 
@@ -223,3 +299,8 @@ init_bdry_cndtn *tri_hp_cd::getnewibc(std::string suffix, input_map& inmap) {
 	temp->input(inmap,keyword);
 	return(temp);
 }
+
+
+
+
+
